c33386c8d0665fb46f5968aa42c84323d5dc367b
alu0100537116 <alu0100537116@ull.edu.es>
2014-10-17 13:02:12 +0000

    Creado metodo de suma, test para suma y comprb ado

---
+++ src/complejos_tb.rb
@@ -0,0 +1,19 @@
+require "../doc/complejos.rb"
+require 'test/unit'
+
+class TestClase < Test::Unit::TestCase
+
+	def setup
+		@C1=Complejos.new(1,1)
+
+	end
+
+	def tear_down
+
+	end
+
+	def test_simple
+		assert_equal("(1,1)",@C1.to_s)
+		assert_equal("(2,2)",@C1.suma(1,1).to_s)
+	end
+end
